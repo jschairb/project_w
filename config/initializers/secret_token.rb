da96ddfe672166b04e2bafa2c5d6eb94473cf9da
Josh Schairbaum <josh.schairbaum@rackspace.com>
2012-04-18 10:46:27 -0500

    initial commit, empty rails skeleton for a project that might become the foundation of a new humanity

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WelzeinWay::Application.config.secret_token = 'b852ec6253b917126081369c8be4c3c3feee447ce0df8b1030b939c44d58e85fcfb6476a3a5e522573be48c6e193c502ea257cf859e973c48c9b4900069c73ea'
